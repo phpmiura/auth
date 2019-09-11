89f69da608d4c43e1a5985b1549320df9ede3766
charly-PC\charly <charly@ipp.ph>
2019-09-11 14:18:22 +0800

    update phpmiura auth

--- src/routes/web.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-Route::group(['namespace' => 'PHPMiura\Auth\Http\Controllers', 'middleware' => ['web']], function(){
-    Route::get('auth', 'AuthController@index');
-    Route::post('auth', 'AuthController@sendMail')->name('auth');
-});
